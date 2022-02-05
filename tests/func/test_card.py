80abe2f954a37aad28d4daced4134035de283b79
yuta3003 <y.nakazawa3003+github@gmail.com>
2022-02-05 23:04:44 +0900

    update

--- tests/func/test_card.py
+++ tests/func/test_card.py
@@ -7,14 +7,19 @@ def test_suit():
     for i in ["C", "D", "H", "S"]:
         for j in range(1, 14):
             test_card = card.Card(i, j)
-            assert card.Card.suit(test_card) == i
+            assert test_card.suit == i
 
 
 def test_number():
     for i in ["C", "D", "H", "S"]:
         for j in range(1, 14):
             test_card = card.Card(i, j)
-            assert card.Card.number(test_card) == j
+            assert test_card.number(ace14=False) == j
+
+    for i in ["C", "D", "H", "S"]:
+        for j in range(2, 15):
+            test_card = card.Card(i, j)
+            assert test_card.number(ace14=True) == j
 
 
 def test_show():
