af3daa29c968f215af8a5ffd21d91cc9d303284e
yuta3003 <y.nakazawa3003+github@gmail.com>
2022-02-06 05:01:20 +0900

    create conftest.py

--- tests/func/test_role.py
+++ tests/func/test_role.py
@@ -8,6 +8,10 @@ from src import (
 )
 
 
+def test_update_role():
+    pass
+
+
 def test_judge_pair():
     pass
 
@@ -36,7 +40,8 @@ def test_judge_straight_flush():
     pass
 
 
-def test_how_many_same_numbers():
+
+def test_how_many_same_numbers1():
     """
     ----------------------------------
      same number = 1
@@ -57,6 +62,7 @@ def test_how_many_same_numbers():
 
     assert test_number == expected_number
 
+def test_how_many_same_numbers2():
 
     """
     ----------------------------------
@@ -78,6 +84,7 @@ def test_how_many_same_numbers():
 
     assert test_number == expected_number
 
+def test_how_many_same_numbers3():
     """
     ----------------------------------
      same number = 3
@@ -98,6 +105,7 @@ def test_how_many_same_numbers():
 
     assert test_number == expected_number
 
+def test_how_many_same_numbers4():
     """
     ----------------------------------
      same number = 4
@@ -118,242 +126,50 @@ def test_how_many_same_numbers():
 
     assert test_number == expected_number
 
-
-def test_judge():
-    """
-    ----------------------------------
-     A Pair
-    ----------------------------------
-    """
-    test_card = []
-    test_card.append(card.Card('C', 1))
-    test_card.append(card.Card('C', 3))
-    test_card.append(card.Card('H', 3))
-    test_card.append(card.Card('S', 6))
-    test_card.append(card.Card('H', 7))
-    test_card.append(card.Card('D', 8))
-    test_card.append(card.Card('S', 12))
-
-    test = role.RoleJudge()
-    test.judge(test_card)
-    test_role = test.role
+def test_judge_one_pair(one_pair):
+    test_role = one_pair.role
     assert test_role == 1
 
-    """
-    ----------------------------------
-     Two Pairs
-    ----------------------------------
-    """
-    test_card = []
-    test_card.append(card.Card('C', 1))
-    test_card.append(card.Card('D', 1))
-    test_card.append(card.Card('H', 3))
-    test_card.append(card.Card('S', 3))
-    test_card.append(card.Card('S', 6))
-    test_card.append(card.Card('S', 12))
-    test_card.append(card.Card('H', 13))
-
-    test = role.RoleJudge()
-    test.judge(test_card)
-    test_role = test.role
+def test_judge_two_pair(two_pair):
+    test_role = two_pair.role
     assert test_role == 2
 
-    """
-    ----------------------------------
-     Three Pairs
-    ----------------------------------
-    """
-    test_card = []
-    test_card.append(card.Card('C', 1))
-    test_card.append(card.Card('D', 1))
-    test_card.append(card.Card('H', 3))
-    test_card.append(card.Card('S', 3))
-    test_card.append(card.Card('S', 6))
-    test_card.append(card.Card('S', 12))
-    test_card.append(card.Card('H', 12))
-
-    test = role.RoleJudge()
-    test.judge(test_card)
-    test_role = test.role
-
+def test_judge_three_pair(three_pair):
+    test_role = three_pair.role
     assert test_role == 2
 
-    """
-    ----------------------------------
-     three of kind
-    ----------------------------------
-    """
-    test_card = []
-    test_card.append(card.Card('C', 1))
-    test_card.append(card.Card('D', 2))
-    test_card.append(card.Card('H', 2))
-    test_card.append(card.Card('S', 2))
-    test_card.append(card.Card('D', 3))
-    test_card.append(card.Card('C', 4))
-    test_card.append(card.Card('H', 12))
-
-    test = role.RoleJudge()
-    test.judge(test_card)
-    test_role = test.role
-
+def test_judge_three_of_kind(three_of_kind):
+    test_role = three_of_kind.role
     assert test_role == 3
 
-    """
-    ----------------------------------
-     straight 1 2 3 4 5
-    ----------------------------------
-    """
-    test_card = []
-    test_card.append(card.Card('C', 1))
-    test_card.append(card.Card('S', 2))
-    test_card.append(card.Card('D', 3))
-    test_card.append(card.Card('H', 4))
-    test_card.append(card.Card('D', 5))
-    test_card.append(card.Card('H', 9))
-    test_card.append(card.Card('C', 13))
-
-    test = role.RoleJudge()
-    test.judge(test_card)
-    test_role = test.role
-
+def test_judge_straight1(straight1):
+    test_role = straight1.role
     assert test_role == 4
 
-    """
-    ----------------------------------
-     straight 5 6 7 8 9
-    ----------------------------------
-    """
-    test_card = []
-    test_card.append(card.Card('C', 1))
-    test_card.append(card.Card('S', 5))
-    test_card.append(card.Card('D', 6))
-    test_card.append(card.Card('H', 7))
-    test_card.append(card.Card('D', 8))
-    test_card.append(card.Card('H', 9))
-    test_card.append(card.Card('C', 13))
-
-    test = role.RoleJudge()
-    test.judge(test_card)
-    test_role = test.role
-
+def test_judge_straight2(straight2):
+    test_role = straight2.role
     assert test_role == 4
 
-    """
-    ----------------------------------
-     straight 10 11 12 13 1
-    ----------------------------------
-    """
-    test_card = []
-    test_card.append(card.Card('C', 1))
-    test_card.append(card.Card('S', 5))
-    test_card.append(card.Card('D', 6))
-    test_card.append(card.Card('H', 10))
-    test_card.append(card.Card('D', 11))
-    test_card.append(card.Card('H', 12))
-    test_card.append(card.Card('C', 13))
-
-    test = role.RoleJudge()
-    test.judge(test_card)
-    test_role = test.role
-
+def test_judge_straight3(straight3):
+    test_role = straight3.role
     assert test_role == 4
 
-    """
-    ----------------------------------
-     flush
-    ----------------------------------
-    """
-    test_card = []
-    test_card.append(card.Card('C', 1))
-    test_card.append(card.Card('C', 2))
-    test_card.append(card.Card('C', 5))
-    test_card.append(card.Card('C', 7))
-    test_card.append(card.Card('C', 8))
-    test_card.append(card.Card('C', 9))
-    test_card.append(card.Card('C', 13))
-
-    test = role.RoleJudge()
-    test.judge(test_card)
-    test_role = test.role
-
+def test_judge_flush(flush):
+    test_role = flush.role
     assert test_role == 5
 
-    """
-    ----------------------------------
-     fullhouse
-    ----------------------------------
-    """
-    test_card = []
-    test_card.append(card.Card('C', 1))
-    test_card.append(card.Card('S', 1))
-    test_card.append(card.Card('D', 1))
-    test_card.append(card.Card('H', 7))
-    test_card.append(card.Card('D', 7))
-    test_card.append(card.Card('C', 7))
-    test_card.append(card.Card('C', 13))
-
-    test = role.RoleJudge()
-    test.judge(test_card)
-    test_role = test.role
-
+def test_judge_fullhouse(fullhouse):
+    test_role = fullhouse.role
     assert test_role == 6
 
-    """
-    ----------------------------------
-     four of kind
-    ----------------------------------
-    """
-    test_card = []
-    test_card.append(card.Card('C', 1))
-    test_card.append(card.Card('D', 2))
-    test_card.append(card.Card('H', 2))
-    test_card.append(card.Card('S', 2))
-    test_card.append(card.Card('C', 2))
-    test_card.append(card.Card('C', 4))
-    test_card.append(card.Card('H', 12))
-
-    test = role.RoleJudge()
-    test.judge(test_card)
-    test_role = test.role
-
+def test_judge_four_of_kind(four_of_kind):
+    test_role = four_of_kind.role
     assert test_role == 7
 
-    """
-    ----------------------------------
-     straight flush
-    ----------------------------------
-    """
-    test_card = []
-    test_card.append(card.Card('H', 1))
-    test_card.append(card.Card('C', 2))
-    test_card.append(card.Card('C', 6))
-    test_card.append(card.Card('C', 7))
-    test_card.append(card.Card('C', 8))
-    test_card.append(card.Card('C', 9))
-    test_card.append(card.Card('C', 10))
-
-    test = role.RoleJudge()
-    test.judge(test_card)
-    test_role = test.role
-
+def test_judge_straight_flush(straight_flush):
+    test_role = straight_flush.role
     assert test_role == 8
 
-    """
-    ----------------------------------
-     royal flush
-    ----------------------------------
-    """
-    test_card = []
-    test_card.append(card.Card('C', 1))
-    test_card.append(card.Card('C', 2))
-    test_card.append(card.Card('C', 6))
-    test_card.append(card.Card('C', 10))
-    test_card.append(card.Card('C', 11))
-    test_card.append(card.Card('C', 12))
-    test_card.append(card.Card('C', 13))
-
-    test = role.RoleJudge()
-    test.judge(test_card)
-    test_role = test.role
-
+def test_judge_royal_flush(royal_flush):
+    test_role = royal_flush.role
     assert test_role == 9
