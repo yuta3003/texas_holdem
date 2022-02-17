4821480a07189c32cc68b08ddaf95b0308c38a3e
yuta3003 <y.nakazawa3003+github@gmail.com>
2022-02-17 11:16:40 +0900

    __init__.pyにimport を入れていたがいらないので削除

--- texas_holdem/__init__.py
+++
@@ -1,7 +0,0 @@
-# import texas_holdem.card
-# import texas_holdem.deck
-# import texas_holdem.field
-# import texas_holdem.games
-# import texas_holdem.player
-# import texas_holdem.role
-# import texas_holdem.winner
